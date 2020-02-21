63d5dc94f9233ff4eb553189446b17164155dfe6
Victor Uland <53414263+vuduland@users.noreply.github.com>
2020-02-21 11:22:48 -0600

    create db.js(db connection) and default.json

---
+++ config/db.js
@@ -0,0 +1,36 @@
+/** @format */
+
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+
+  //   mongoose  // without async await
+  //     .connect(db, {
+  //       useNewUrlParser: true,
+  //       useCreateIndex: true,
+  //       useFindAndModify: false,
+  //       useUnifiedTopology: true,
+  //     })
+  //     .then(() => console.log('MongoDB Connected'))
+  //     .catch(err => {
+  //       console.error(err.message);
+  //       process.exit(1);
+  //     });
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 /** @format */
 
 const express = require('express'); // must use this which is called CommonJS syntax, unless using typescript or babel
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+// Connect Database
+connectDB();
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API...' })
 );
